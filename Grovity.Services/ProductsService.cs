343bf951bf722f93b7d31be61edcee44c315d3ce
Thamin174 <thaminnaik@gmail.com>
2020-06-04 15:04:35 +0530

    CRUD operation completed using Ajax jQuery

---
+++ Grovity.Services/ProductsService.cs
@@ -0,0 +1,60 @@
+﻿using Grovity.Database;
+using Grovity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grovity.Services
+{
+     public class ProductsService
+    {
+        public Product GetProduct(int ID)
+        {
+            using (var context = new GrovityContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+        public List<Product> GetProducts()
+        {
+            using (var context = new GrovityContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+
+        public void SaveProduct(Product product)
+        {
+            using (var context = new GrovityContext())
+            {
+                context.Products.Add(product);
+
+                context.SaveChanges();
+            }
+        }
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new GrovityContext())
+            {
+                context.Entry(product).State = EntityState.Modified;
+
+                context.SaveChanges();
+            }
+        }
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new GrovityContext())
+            {
+                var product = context.Products.Find(ID);
+
+                context.Products.Remove(product);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Grovity.Web/Controllers/ProductController.cs
@@ -0,0 +1,67 @@
+﻿using Grovity.Entities;
+using Grovity.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Grovity.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductTable(string search)
+        {
+            var products = productsService.GetProducts();
+
+            if(string.IsNullOrEmpty(search)==false)
+            {
+                products = products.Where(p => p.Name != null && p.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+           
+
+            return PartialView(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+
+            return RedirectToAction("ProductTable");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var product = productsService.GetProduct(ID);
+
+            return PartialView(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            productsService.UpdateProduct(product);
+
+            return RedirectToAction("ProductTable");
+        }
+        [HttpPost]
+        public ActionResult Delete(int ID)
+        {
+            productsService.DeleteProduct(ID);
+
+            return RedirectToAction("ProductTable");
+        }
+
+    }
+}
