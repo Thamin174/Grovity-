33353a70ed24bc53edb421a6bb052931f9366304
Thamin174 <thaminnaik@gmail.com>
2020-06-04 08:58:24 +0530

    Add project files.

---
+++ Grovity.Database/GrovityContext.cs
@@ -0,0 +1,20 @@
+﻿using Grovity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grovity.Database
+{
+    public class GrovityContext : DbContext,IDisposable
+    {
+        public GrovityContext() : base("Grovity")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Grovity.Services/CategoryService.cs
@@ -0,0 +1,60 @@
+﻿using Grovity.Database;
+using Grovity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grovity.Services
+{
+     public class CategoryService
+    {
+        public Category GetCategory(int ID)
+        {
+            using (var context = new GrovityContext())
+            {
+                return context.Categories.Find(ID);
+            }
+        }
+        public List<Category> GetCategories()
+        {
+            using (var context = new GrovityContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+
+        public void SaveCategory(Category category)
+        {
+            using (var context = new GrovityContext())
+            {
+                context.Categories.Add(category);
+
+                context.SaveChanges();
+            }
+        }
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new GrovityContext())
+            {
+                context.Entry(category).State = EntityState.Modified;
+
+                context.SaveChanges();
+            }
+        }
+        public void DeleteCategory(int ID)
+        {
+            using (var context = new GrovityContext())
+            {
+                var category = context.Categories.Find(ID);
+
+                context.Categories.Remove(category);
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
